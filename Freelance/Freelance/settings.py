25e9966c9ab870515b7fe252bdda3cf7270ba371
BibaBlloba <vladvaledol@gmail.com>
2024-12-25 17:43:36 +0300

    csrf

--- Freelance/Freelance/settings.py
+++ Freelance/Freelance/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!fy)j%vos=cq_=&h842c)4!jq@927xl0is(=s!nzd0zxjj$0&-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = ['https://vlados.akeka.ru']
 
 
 # Application definition
