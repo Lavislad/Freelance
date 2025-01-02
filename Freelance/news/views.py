0586962207c503b1c71ceee1572d495e3b9fcaef
Vladislav <vladislav.mnac@gmail.com>
2025-01-02 20:16:00 +0300

    WIP

--- Freelance/news/views.py
+++ Freelance/news/views.py
@@ -4,6 +4,7 @@ from .models import Articles
 def news_index(request):
     news = Articles.objects.all()
     data = {
-        'title': 'News Page'
+        'title': 'News Page',
+        'news': news
     }
     return render(request, 'news/news_index.html', data)
