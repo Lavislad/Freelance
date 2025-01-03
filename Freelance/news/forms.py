a70b68ed8d9348c32264d5313a6751bb5912a3a5
Vladislav <vladislav.mnac@gmail.com>
2025-01-03 15:35:20 +0300

    News Form

---
+++ Freelance/news/forms.py
@@ -0,0 +1,23 @@
+from .models import Articles
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
+
+
+class ArticlesForm(ModelForm):
+    class Meta:
+        model = Articles
+        fields = ['title', 'anons', 'full_text', 'date']
+
+        widgets = {
+            'title': TextInput(attrs={
+                'placeholder': 'Title'
+            }),
+            'anons': TextInput(attrs={
+                'placeholder': 'Anons'
+            }),
+            'full_text': Textarea(attrs={
+                'placeholder': 'Text'
+            }),
+            'date': DateTimeInput(attrs={
+                'placeholder': 'Date'
+            })
+        }
--- Freelance/news/views.py
+++ Freelance/news/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
 
 def news_index(request):
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_index')
+        else:
+            error = 'Form is invalid'
     news = Articles.objects.order_by('-date')
+    form = ArticlesForm()
     data = {
         'title': 'News Page',
-        'news': news
+        'news': news,
+        'form': form,
+        'error': error
     }
     return render(request, 'news/news_index.html', data)
