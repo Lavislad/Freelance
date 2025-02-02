a1bcc6131b710b0bae2dc67258928dd8be4f2f62
Vladislav <vladislav.mnac@gmail.com>
2025-02-02 12:09:33 +0300

    News and Feedback Pages Designed

--- Freelance/account/models.py
+++ Freelance/account/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Аватар')
+    # identifier = models.CharField(max_length=40, unique=True)
+    # USERNAME_FIELD = "identifier"
 
     class Meta:
         db_table = 'user'
