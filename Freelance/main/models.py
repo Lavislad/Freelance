efe23a286ccb9d29a5a7462c2c9b8130fa22c49a
Vladislav <vladislav.mnac@gmail.com>
2025-04-23 13:49:45 +0300

    Migration

--- Freelance/main/models.py
+++ Freelance/main/models.py
@@ -1,20 +1,30 @@
 from django.db import models
 from account.models import User
 
+
+class Tags(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+
+
 class Vacancy(models.Model):
     title = models.CharField(max_length=200)
     payment = models.CharField()
     description = models.TextField(max_length=1500)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tags, related_name='vacancies', verbose_name='Теги')
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = "Вакансия"
         verbose_name_plural = "Вакансии"
 
-class Tags(models.Model):
-    title = models.CharField(max_length=50)
-
-    class Meta:
-        verbose_name = "Тег"
-        verbose_name_plural = "Теги"
