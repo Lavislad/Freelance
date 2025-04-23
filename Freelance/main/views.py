a152d586c45eaf515b7cf46d31c69d3b5f66d63e
Vladislav <vladislav.mnac@gmail.com>
2025-04-23 14:20:47 +0300

    tag filter

--- Freelance/main/views.py
+++ Freelance/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Vacancy
+from .models import Vacancy, Tags
 
 
 def index(request):
@@ -16,8 +16,16 @@ def index(request):
     return render(request, 'main/index.html', data)
 
 def vacancy_list(request):
+    tag_slug = request.GET.get('tag')
     vacancies = Vacancy.objects.all().prefetch_related('tags')
+    all_tags = Tags.objects.all()
+
+    if tag_slug:
+        vacancies = vacancies.filter(tags__title=tag_slug)
+
     data = {
         'vacancies': vacancies,
+        'all_tags': all_tags,
+        'current_tag': tag_slug,
     }
     return render(request, 'main/vacancy_list.html', data)
