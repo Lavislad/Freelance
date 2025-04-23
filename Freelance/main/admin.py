502893c6d3751c953e1bd118fae2930c7b09a85f
Vladislav <vladislav.mnac@gmail.com>
2025-04-23 14:13:22 +0300

    tags

--- Freelance/main/admin.py
+++ Freelance/main/admin.py
@@ -4,13 +4,13 @@ from .models import Vacancy, Tags
 
 @admin.register(Tags)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('title',)
 
 @admin.register(Vacancy)
 class VacancyAdmin(admin.ModelAdmin):
     list_display = ('title', 'display_tags')
 
     def display_tags(self, obj):
-        return ", ".join([tag.name for tag in obj.tags.all()])
+        return ", ".join([tag.title for tag in obj.tags.all()])
 
     display_tags.short_description = 'Теги'
