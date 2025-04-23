d009375185404cfd078edd40f9243aeb7afb7d1b
Vladislav <vladislav.mnac@gmail.com>
2025-04-23 19:20:39 +0300

    tags styles

--- Freelance/main/views.py
+++ Freelance/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Vacancy, Tags
+from django.db.models import Q
 
 
 def index(request):
@@ -11,16 +12,19 @@ def index(request):
     return render(request, 'main/index.html', data)
 
 def vacancy_list(request):
-    tag_slug = request.GET.get('tag')
+    selected_tags = request.GET.getlist('tags')
     vacancies = Vacancy.objects.all().prefetch_related('tags').order_by('-date')
     all_tags = Tags.objects.all()
 
-    if tag_slug:
-        vacancies = vacancies.filter(tags__title=tag_slug)
+    if selected_tags:
+        q_objects = Q()
+        for tag in selected_tags:
+            q_objects &= Q(tags__title=tag)
+        vacancies = vacancies.filter(q_objects).distinct()
 
     data = {
         'vacancies': vacancies,
         'all_tags': all_tags,
-        'current_tag': tag_slug,
+        'selected_tags': selected_tags,
     }
     return render(request, 'main/vacancy_list.html', data)
